78ca0cf4c79f8e239269e15cb36da786d81038f1
Nihit <nihitavr@gmail.com>
2016-07-23 12:10:39 +0530

    first commit

---
+++ src/dashboard/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+console.log(data);
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <h1>Dashboard</h1>
+        )
+    }
+}
+
+
+export default Dashboard
